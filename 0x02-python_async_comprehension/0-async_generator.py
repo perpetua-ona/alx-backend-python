f850679d265305e025f0daa30201669c3c0bcf79
perpetua-ona <onaperpetua@gmail.com>
2024-10-16 03:11:04 +0000

    All done

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Task 0
+'''
+import asyncio
+import random
+from typing import Generator
+# Generator[yield_type, send_type, return_type]
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''Coroutine that generates a sequence of 10 numbers.
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
